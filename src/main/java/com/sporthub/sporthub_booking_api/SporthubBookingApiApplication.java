d52d040e10dff94821e409893b3265b25c632af4
Wytse Van Stichel <wytse.vanstichel@student.vives.be>
2026-07-07 05:20:51 +0200

    Initial Spring Boot project

---
+++ src/main/java/com/sporthub/sporthub_booking_api/SporthubBookingApiApplication.java
@@ -0,0 +1,13 @@
+package com.sporthub.sporthub_booking_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SporthubBookingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SporthubBookingApiApplication.class, args);
+	}
+
+}
